571f266a35ba21269cb51ddd8a5fa9af95a8944f
redjax <jackenyon@gmail.com>
2024-03-14 00:40:51 -0400

    Rework project so local dependencies from other dirs work.

---
+++ sandbox/projects/test/main.py
@@ -0,0 +1,5 @@
+from core.constants import SPACETRADERS_API_BASE_URL, SPACETRADERS_API_V2_BASE
+import register_agent
+
+if __name__ == "__main__":
+    agent_symbol = register_agent.random_agentname()
